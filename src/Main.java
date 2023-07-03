e8efc4074ecef607638ee51af000f737482c7fd8
manish acharya <mansih.acharya313@gmail.com>
2023-07-03 14:42:48 +0545

    simple ui in java

---
+++ src/Main.java
@@ -0,0 +1,112 @@
+//import javax.swing.JLabel;
+//import javax.swing.JFrame;
+//import javax.swing.JPanel;
+//import javax.swing.JButton;
+//import javax.swing.JTextField;
+//import java.awt.*;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//
+//        //panal
+//        JPanel Ppanel = new JPanel();
+//        Ppanel.setBounds(0,0,500,50);
+//        Ppanel.setLayout(null);
+////        Ppanel.setBackground(Color.red);
+//
+//        JPanel Tpanel = new JPanel();
+//        Tpanel.setBounds(0,50,500,50);
+////        Tpanel.setBackground(Color.green);
+//        Tpanel.setLayout(null);
+//
+//
+//        JPanel Rpanel = new JPanel();
+//        Rpanel.setBounds(0,100,500,50);
+////        Rpanel.setBackground(Color.yellow);
+//        Rpanel.setLayout(null);
+//
+//        JPanel Ipanel = new JPanel();
+//        Ipanel.setBounds(0,150,500,50);
+////        Ipanel.setBackground(Color.blue);
+//        Ipanel.setLayout(null);
+//
+//        JPanel ButtonPanel = new JPanel();
+//        ButtonPanel.setBounds(0,200,500,50);
+////        ButtonPanel.setBackground(Color.gray);
+//
+//
+//        //label
+//        JLabel Plabel = new JLabel();
+//        Plabel.setText("P");
+//        Plabel.setBounds(10,10,10,10);
+//        JLabel Tlabel = new JLabel();
+//        Tlabel.setText("t");
+//        Tlabel.setBounds(10,10,10,10);
+//
+//        JLabel Rlabel = new JLabel();
+//        Rlabel.setText("R");
+//        Rlabel.setBounds(10,10,10,10);
+//
+//
+//        JLabel Ilabel = new JLabel();
+//        Ilabel.setText("I");
+//        Ilabel.setBounds(10,10,10,10);
+//
+//        //button
+//        JButton button = new JButton();
+//        button.setBounds(100,100,80,30);
+//        button.setText("OK");
+//
+//        //textfield
+//        JTextField textField = new JTextField();
+//        textField.setSize(new Dimension(250,40));
+//        textField.setBounds(30,10,250,40);
+//
+//        JTextField textField1 = new JTextField();
+//        textField1.setSize(new Dimension(250,40));
+//        textField1.setBounds(30,10,250,40);
+//
+//        JTextField textField2 = new JTextField();
+//        textField2.setSize(new Dimension(250,40));
+//        textField2.setBounds(30,10,250,40);
+//
+//        JTextField textField3 = new JTextField();
+//        textField3.setSize(new Dimension(250,40));
+//        textField3.setBounds(30,10,250,40);
+//
+//
+//
+//        //frame
+//        JFrame frame = new JFrame();
+//        frame.setVisible(true);
+//        frame.setSize(500,400);
+//        frame.setLayout(null);
+//        frame.add(Ppanel);
+//        frame.add(Tpanel);
+//        frame.add(Rpanel);
+//        frame.add(Ipanel);
+//        //
+//        Ppanel.add(Plabel);
+//        Ppanel.add(textField);
+//        //
+//        Rpanel.add(Rlabel);
+//        Rpanel.add(textField1);
+//        //
+//        Tpanel.add(Tlabel);
+//        Tpanel.add(textField2);
+//        //
+//        Ipanel.add(Ilabel);
+//        Ipanel.add(textField3);
+//        //
+//        frame.add(ButtonPanel);
+//        ButtonPanel.add(button);
+//
+//    }
+//}
+
+public class Main {
+
+    public static void main(){
+        System.out.println("hey");
+    }
+}
